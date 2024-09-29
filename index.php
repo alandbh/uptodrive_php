728bff3a12f5d4fda7c2557f9ac9bf6b84505cc0
Alan Vasconcelos <alandbh@gmail.com>
2024-09-28 22:56:51 -0300

    adding local url

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ set_time_limit(120);
 
 // Habilitar CORS para as requisições OPTIONS (preflight)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: http://localhost:3003');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
     header('Access-Control-Max-Age: 86400'); // Cache a resposta por um dia
