326c3ea6ee6f2cb5a128ff81f7294a05434ae0cc
Alan Vasconcelos <alandbh@gmail.com>
2024-09-29 00:41:57 -0300

    changing headers

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ require 'vendor/autoload.php';
 // Habilitar CORS para as requisições OPTIONS (preflight)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: http://localhost:3003');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
-    header('Access-Control-Max-Age: 86400'); // Cache a resposta por um dia
-    exit(0); // Finaliza a requisição OPTIONS
+    header('Access-Control-Allow-Credentials: true'); // Se necessário
+    exit(0);
 }
 
 use Google\Client;
@@ -34,11 +34,10 @@ $service = new Google_Service_Drive($client);
 
 // Upload de arquivo
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST');
-    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
-    header('Content-Type: application/json');
-    header('Access-Control-Max-Age: 86400'); // Cache a resposta por um dia
+    header('Access-Control-Allow-Origin: http://localhost:3003');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+    header('Access-Control-Allow-Credentials: true'); // Se necessário
 
     // $result_json = array('log' => $_FILES['file'] );
 
